31e77d0a66dfbafce4f59e96e98b682af116be0b
Your name here <brkmer@gmail.com>
2022-09-15 12:01:49 -0400

    location box added, dateBuilder function added

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,8 +4,13 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/"
 }
 function App() {
+
+  const dateBuilder = (d) => {
+    let months = ["January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  }
   return (
-    <div className="app">
+    <div className="app ">
       <main>
         <div className="search-box"> 
           <input
@@ -14,6 +19,11 @@ function App() {
             placeholder="Search..."
             />
          </div>
+        <div className="location-box">
+          <div className="location">orlando, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+          
+        </div>
       </main>
     </div>
   );
