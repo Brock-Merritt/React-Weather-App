98dc671c8a66db1c0134b97f8877d0233c0e9b1e
Your name here <brkmer@gmail.com>
2022-09-19 19:58:20 -0400

    setQuery to (), added input onchange value onkeypress

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -11,7 +11,12 @@ function App() {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then(res => res.json())
-        .then(result => setWeather(result));
+        .then(result =>{
+           setWeather(result);
+           setQuery('');
+           console.log(weather)
+           
+        });
     }
   }
 
@@ -34,6 +39,9 @@ function App() {
             type="text"
             className="search-bar"
             placeholder="Search..."
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
             />
          </div>
         <div className="location-box">
